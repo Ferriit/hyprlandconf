a7e8d46fbe3c474af6b93d146d6df205a45b5762
Ferriit <ferriitalt@gmail.com>
2025-02-03 18:02:21 +0100

    Create debianinstall.sh

---
+++ debianinstall.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+sudo su
+apt install hyprland
+apt install alacritty
+apt install dolphin
+apt install wofi
+apt install waybar
+apt install hyprpaper
+apt install xorg-x11-server-Xwayland

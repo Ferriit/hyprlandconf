6037286a695c620eee150626b4d03c90a4afee92
Ferriit <ferriitalt@gmail.com>
2025-02-03 18:01:37 +0100

    Create fedorainstall.sh

---
+++ fedorainstall.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+sudo su
+spkg install hyprland
+spkg install alacritty
+spkg install dolphin
+spkg install wofi
+spkg install waybar
+spkg install hyprpaper
+spkg install xorg-x11-server-Xwayland
